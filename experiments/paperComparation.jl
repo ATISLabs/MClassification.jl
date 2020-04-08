2b3d0375096dca23973095233c0ab536f7d4fbbc
Conrado <pedrooconradoo@gmail.com>
2020-04-08 14:19:42 -0300

    fix experiments erros

--- experiments/paperComparation.jl
+++ experiments/paperComparation.jl
@@ -8,9 +8,8 @@ path = "../datasets/sinthetic/"
 datasets = split(read(`ls $path`, String), "\n")
 
 dataset_results = DataFrame()
-
-for dataset in datasets[1:6]
-    n_instancias = 0
+for dataset in datasets[1:10]
+    n_instances = 0
     hits = 0
     misses = 0
 
@@ -20,14 +19,13 @@ for dataset in datasets[1:6]
     dataset_file = open(path * dataset , "r")
     MClassifier_initialize(classifier, dataset_file, 150)
 
-    #while true
-    for i in 1:15000
-        n_instancias += 1
+    while true
         new_instance = zeros(Float64, classifier.n_features)
         rel1 = load_instance(dataset_file, 2, new_instance)
         if rel1 == ""
             break
         end
+        n_instances += 1
         rel2 = MClassifier_predict(classifier, new_instance)
         if rel1 == rel2
             hits += 1
@@ -36,10 +34,9 @@ for dataset in datasets[1:6]
         end
     end
 
-    x = hits / n_instancias
+    x = hits / n_instances
 
-    dataset_result = DataFrame(dataset_name =  dataset, acurracy = x)
+    dataset_result = DataFrame(dataset =  dataset, n_initial_instances = 150 ,n_samples = n_instances, accuracy = round(x, digits=2, RoundDown))
     append!(dataset_results, dataset_result)
 end
-
 println(dataset_results)
